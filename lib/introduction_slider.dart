743bb7b1992e5bb996d71c7f7386ae2926b9f722
Rahul Chouhan <rahul-chouhan@outlook.com>
2022-09-30 11:23:52 +0530

    Modified this file

--- lib/introduction_slider.dart
+++ lib/introduction_slider.dart
@@ -1,7 +1,3 @@
 library introduction_slider;
 
-export 'src/introduction_slider.dart';
-export 'utils/introduction_slider_item.dart';
-export 'utils/background_decoration.dart';
-export 'utils/buttons.dart';
-export 'utils/dot_indicator.dart';
+export 'source/source.dart';
