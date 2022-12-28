f3a39f862c639493b4dee866a14d63002fdc18d1
lucas <lucas.cardo123@hotmail.com>
2022-12-28 14:27:11 -0400

    Add onPressed in done button

--- lib/source/presentation/pages/introduction_slider.dart
+++ lib/source/presentation/pages/introduction_slider.dart
@@ -177,35 +177,36 @@ class _IntroductionSliderState extends State<IntroductionSlider> {
                         ),
                   lastIndex
                       ? TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                              PageRouteBuilder(
-                                transitionDuration:
-                                    widget.done.animationDuration!,
-                                transitionsBuilder: (context, animation,
-                                    secondAnimation, child) {
-                                  animation = CurvedAnimation(
-                                    parent: animation,
-                                    curve: widget.done.curve!,
-                                  );
-                                  return SlideTransition(
-                                    position: Tween<Offset>(
-                                      begin: widget.scrollDirection ==
-                                              Axis.vertical
-                                          ? const Offset(0, 1.0)
-                                          : const Offset(1.0, 0.0),
-                                      end: Offset.zero,
-                                    ).animate(animation),
-                                    child: child,
-                                  );
-                                },
-                                pageBuilder:
-                                    (context, animation, secondaryAnimation) {
-                                  return widget.done.home!;
-                                },
-                              ),
-                            );
-                          },
+                          onPressed: widget.done.onPressed ??
+                              () {
+                                Navigator.of(context).pushReplacement(
+                                  PageRouteBuilder(
+                                    transitionDuration:
+                                        widget.done.animationDuration!,
+                                    transitionsBuilder: (context, animation,
+                                        secondAnimation, child) {
+                                      animation = CurvedAnimation(
+                                        parent: animation,
+                                        curve: widget.done.curve!,
+                                      );
+                                      return SlideTransition(
+                                        position: Tween<Offset>(
+                                          begin: widget.scrollDirection ==
+                                                  Axis.vertical
+                                              ? const Offset(0, 1.0)
+                                              : const Offset(1.0, 0.0),
+                                          end: Offset.zero,
+                                        ).animate(animation),
+                                        child: child,
+                                      );
+                                    },
+                                    pageBuilder: (context, animation,
+                                        secondaryAnimation) {
+                                      return widget.done.home!;
+                                    },
+                                  ),
+                                );
+                              },
                           style: widget.done.style,
                           child: widget.done.child,
                         )
